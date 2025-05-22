688d718a1405174b5dfbac4e2ac1092910eac6cf
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2025-05-22 15:11:51 -0500

    Add a function to gather detailed funding information

--- src/vc-dd/get-financials.ts
+++ src/vc-dd/get-financials.ts
@@ -9,3 +9,17 @@ const fetchCrunchbase = async (company: string) => {
   });
   return result.results[0];
 };
+
+const fetchFunding = async (company: string) => {
+  const result = await exa.searchAndContents(`${company} Funding:`, {
+    type: "keyword",
+    numResults: 1,
+    text: true,
+    summary: {
+      query: `Tell me about the funding (and if available, the valuation) of this company in detail. Do not tell me about the company, just give all the funding information in detail. If funding or valuation info is not preset, just reply with one word "NO".`,
+    },
+    livecrawl: "always",
+    includeText: [company],
+  });
+  return result.results[0];
+};
