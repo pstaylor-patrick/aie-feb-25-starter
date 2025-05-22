57f0729021e4fbb91e0c78eb77f63bfe836454b3
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2025-05-22 13:59:22 -0500

    Experimental output with generateText (this allows you to use tools with structured outputs)

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
-import { openai } from "@ai-sdk/openai";
-import { generateText, tool } from "ai";
-import "dotenv/config";
-import { z } from "zod";
+import { openai } from '@ai-sdk/openai'
+import { generateText, Output, tool } from 'ai'
+import 'dotenv/config'
+import { z } from 'zod'
  
 const main = async () => {
   const result = await generateText({
-    model: openai("gpt-4o"),
-    prompt: "Get the weather in SF and NY, then add them together.",
+    model: openai('gpt-4o'),
+    prompt: 'Get the weather in SF and NY, then add them together.',
     maxSteps: 3,
     tools: {
       addNumbers: tool({
-        description: "Add two numbers together",
+        description: 'Add two numbers together',
         parameters: z.object({
           num1: z.number(),
           num2: z.number(),
         }),
         execute: async ({ num1, num2 }) => {
-          return num1 + num2;
+          return num1 + num2
         },
       }),
       getWeather: tool({
-        description: "Get the current weather at a location",
+        description: 'Get the current weather at a location',
         parameters: z.object({
           latitude: z.number(),
           longitude: z.number(),
@@ -28,22 +28,25 @@ const main = async () => {
         }),
         execute: async ({ latitude, longitude, city }) => {
           const response = await fetch(
-            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weathercode,relativehumidity_2m&timezone=auto`,
-          );
+            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weathercode,relativehumidity_2m&timezone=auto`
+          )
  
-          const weatherData = await response.json();
+          const weatherData = await response.json()
           return {
             temperature: weatherData.current.temperature_2m,
             weatherCode: weatherData.current.weathercode,
             humidity: weatherData.current.relativehumidity_2m,
             city,
-          };
+          }
         },
       }),
     },
-  });
+    experimental_output: Output.object({
+      schema: z.object({ sum: z.string() }),
+    }),
+  })
   console.log(result.steps.length)
-  console.log(result.text);
-};
+  console.log(result.experimental_output)
+}
  
-main();
+main()
