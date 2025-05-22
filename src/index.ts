7cd589d1ac6ebde3690a62ab25bd5e61e3f1e107
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2025-05-22 13:50:36 -0500

    While we're at it, let's try Google.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { perplexity } from '@ai-sdk/perplexity'
+import { google } from '@ai-sdk/google'
 import { generateText } from 'ai'
 import 'dotenv/config'
  
 const main = async () => {
   const result = await generateText({
-    model: perplexity('sonar'),
+    model: google('gemini-2.0-flash-001', { useSearchGrounding: true }),
     prompt: 'When is the AI Engineer summit?',
   })
   console.log(result.text)
