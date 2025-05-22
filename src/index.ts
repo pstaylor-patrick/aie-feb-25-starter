ac04ebf53868e3a6abea2210f21ae2543a36aad5
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2025-05-22 14:05:58 -0500

    We can use the describe function to help refine the generation.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { openai } from '@ai-sdk/openai'
-import { generateObject } from 'ai'
-import 'dotenv/config'
-import { z } from 'zod'
+import { openai } from "@ai-sdk/openai";
+import { generateObject } from "ai";
+import "dotenv/config";
+import { z } from "zod";
  
 const main = async () => {
   const result = await generateObject({
-    model: openai('gpt-4o-mini'),
-    prompt: 'Please come up with 10 definitions for AI agents.',
+    model: openai("gpt-4o-mini"),
+    prompt: "Please come up with 10 definitions for AI agents.",
     schema: z.object({
-      definitions: z.array(z.string()),
+      definitions: z.array(z.string().describe("Use as much jargon as possible. It should be completely incoherent.")),
     }),
-  })
-  console.log(result.object.definitions)
-}
+  });
+  console.log(result.object.definitions);
+};
  
-main()
+main();
