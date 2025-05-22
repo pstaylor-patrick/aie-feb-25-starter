e3acc8ee1d54128fe0951da390fb5b42361194aa
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2025-05-22 14:48:31 -0500

    Define a shared prompt.

--- src/exa-company-info/index.ts
+++ src/exa-company-info/index.ts
@@ -3,6 +3,14 @@ import Exa from "exa-js";
 
 export const exa = new Exa(process.env.EXA_API_KEY);
 
+const companyInfoPrompt = (
+  company: string,
+) => `For the following company provide:
+- a brief company description
+- what do they sell / what products do they offer
+ 
+<company>${company}</company>`;
+
 const main = async () => {
   console.log("hello world");
 };
